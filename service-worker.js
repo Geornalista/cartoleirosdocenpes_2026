44274a3cf004f1fcff11c6d3cf1cd0dfeab45e82
Geornalista <83824843+Geornalista@users.noreply.github.com>
2026-02-18 01:09:56 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,10 +12,19 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Estratégia de busca: Tenta a rede primeiro, se falhar (offline), usa o cache
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
